a17212d3f13d9e4bb8d7eb4d3d3e58e8ba1282b8
jacobchua <jakubchua98@gmail.com>
2017-07-28 01:03:00 +0800

    Update 4b.sh

--- 4b.sh
+++ 4b.sh
@@ -281,7 +281,7 @@ else
 fi
 
 printf "\e[0m Determine the current settings for reusing of older passwords \n "
-if grep "^remember=5" /etc/pam.d/system-auth >/dev/null; then # Grep "remember=5" from /etc/pam.d/system-auth and remove the output with "/dev/null"
+if grep "remember=5" /etc/pam.d/system-auth >/dev/null; then # Grep "remember=5" from /etc/pam.d/system-auth and remove the output with "/dev/null"
 	printf "\033[33;32m PASS \n"
 else
 	printf "\033[33;31m FAIL \n"
