4e220fc22c1ee90e5aac3edefe23e44a0089a9b0
jacobchua <jakubchua98@gmail.com>
2017-07-30 20:42:07 +0800

    Update 4b.sh

--- 4b.sh
+++ 4b.sh
@@ -88,7 +88,7 @@ fi
 
 #10
 
-printf " Checking if the SSH protocol is correct:  \n"
+printf "\e[0m Checking if the SSH protocol is correct:  \n"
 if grep "^Protocol 2" /etc/ssh/sshd_config > /dev/null; then # Grep "Protocol 2" to ensure settings is correct and remove the output with "/dev/null"
 	printf "\033[33;32m PASS \n"
 else
