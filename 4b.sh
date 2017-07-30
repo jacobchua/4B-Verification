aad85c94fddd76875de253a5a6155cbb2737699e
jacobchua <jakubchua98@gmail.com>
2017-07-30 23:03:43 +0800

    Update 4b.sh

--- 4b.sh
+++ 4b.sh
@@ -79,7 +79,6 @@ else
 	printf "\033[33;31m FAIL \n"
 fi
 
-printf "\e[0m Checking if at is restricted to Authorized Users \n"
 if ls -l /etc/at.allow | grep -e -rw------- >/dev/null; then # Grep the permissions from ls -l /etc/at.allow to ensure permissions is correct and remove the output with "/dev/null"
 	printf "\033[33;32m PASS \n"
 else
